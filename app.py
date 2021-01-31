0499a2b256054bf9eab0d15c83ad23f9ad3b468b
Sergey <58142395+Grayder0152@users.noreply.github.com>
2021-01-31 15:00:42 +0200

    Add mains file

---
+++ app.py
@@ -0,0 +1,36 @@
+import requests
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'asf3f4678jf2e34Ae'
+
+history_website_load_speed = []
+
+
+def speed_test(urls):
+    global history_website_load_speed
+    websites_load_speed = []
+    for url in urls:
+        try:
+            speed = requests.get(url).elapsed.total_seconds()
+            websites_load_speed.append(speed)
+        except:
+            websites_load_speed.append('Incorrect URL')
+    history_website_load_speed += tuple(zip(urls, websites_load_speed))
+    return zip(urls, websites_load_speed)
+
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    if request.method == 'POST':
+        urls = []
+        for name in request.form:
+            urls.append(request.form[name].replace(' ', ''))
+        return render_template('index.html', test=True, urls=set(urls), speed_test=speed_test,
+                               history_website_load_speed=history_website_load_speed)
+    return render_template('index.html', test=False, history_website_load_speed=history_website_load_speed)
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
