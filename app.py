aa8882dd357eea78e46abd2509184c9b34855d45
Sergey <derevankosergej0@gmail.com>
2021-02-01 20:49:00 +0200

    Ассинхронные запросы

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 from datetime import datetime
 
 app = Flask(__name__)
@@ -8,18 +8,24 @@ app.config['SECRET_KEY'] = 'asf3f4678jf2e34Ae'
 history_website_load_speed = ()
 
 
-def speed_test(urls):
+def website_load_speed(urls):
     global history_website_load_speed
-    websites_load_speed = []
     for url in urls:
         try:
             speed = requests.get(url).elapsed.total_seconds()
-            websites_load_speed.append(speed)
+            yield url, speed
         except:
             speed = 'Incorrect URL'
-            websites_load_speed.append('Incorrect URL')
+            yield url, speed
         history_website_load_speed += ((url, str(datetime.now())[:19], speed),)
-    return zip(urls, websites_load_speed)
+
+
+def stream_template(template_name, **context):
+    app.update_template_context(context)
+    t = app.jinja_env.get_template(template_name)
+    rv = t.stream(context)
+    rv.enable_buffering(10)
+    return rv
 
 
 @app.route('/', methods=('GET', 'POST'))
@@ -28,10 +34,11 @@ def index():
         urls = []
         for name in request.form:
             urls.append(request.form[name].replace(' ', ''))
-        return render_template('index.html', test=True, speed_test=speed_test(set(urls)),
-                               history_website_load_speed=history_website_load_speed)
+        url_speed = website_load_speed(set(urls))
+        return Response(stream_template('index.html', test=True, url_speed=url_speed,
+                                       history_website_load_speed=history_website_load_speed))
     return render_template('index.html', test=False, history_website_load_speed=history_website_load_speed)
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
